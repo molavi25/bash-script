107e355c86d3e46085de1204e89b05a0782051ae
molavi25 <alimolavi025@gmail.com>
2023-03-30 21:51:14 +0330

    using command outputs in script

--- best-day-ever.sh
+++ best-day-ever.sh
@@ -7,6 +7,11 @@
 name=$1
 compliment=$2
 
+#using output of some command
+user=$(whoami)
+date=$(date)
+whereami=$(pwd)
+
 echo "Good Morning $name!!"
 
 sleep 1
@@ -16,3 +21,7 @@ echo "you're looking good today $name!!"
 sleep 1
 
 echo "You have the best $compliment I've ever seen $name!!"
+
+sleep 2
+
+echo "You are currently logged in as $user and you are in the directory $whereami. Also today is: $date"
